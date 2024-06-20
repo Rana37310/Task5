17f8fa2b23e96b38702df55b6dcd1f7b21631216
RANA ALBEDAIWI <159438182+Rana37310@users.noreply.github.com>
2024-06-20 17:57:24 +0100

    Update main.php

--- main.php
+++ main.php
@@ -2,16 +2,7 @@
 // Class Description: to present the Fibonacci Sequence
 // Author: Rana Albedaiwi
 // Date : 6/20/2024
-/*
- Create a PHP function (Laravel way) that generates a Fibonacci sequence of a 
-specified length. Explain whether you would use recursion or iteration for this task, and 
-provide your implementation.
 
- I used the iterative approach to generate the Fibonacci sequence because iteration has better time complexity 
- and is less likely to produce errors compared to recursion. 
- Please note that the Laravel implementation is beyond my current skill level, but I would be more than happy 
- to learn it if I had more time.
-*/
 
 
 require_once 'FibonacciSequence.php'; 
